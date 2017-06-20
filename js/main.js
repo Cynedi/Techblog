e5c403380c308c1ee0fa21c3f70ef75f977f2751
cindy <cindy.nicolay@laposte.net>
2017-06-20 11:26:29 +0200

    reseaux sociaux suivez nous page Apropos.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-function "alt"(id) {
-  alert (document.images.identifiant.alt);
-}
+//************Pop'up de confirmation de l'envoi quand on clique sur envoyer**************************//
+
+
+  document.getElementById("myBtn").addEventListener("click", function(){
+      alert("Confirmer l'envoi?");
+  });
+//************Table interactif avec onglet**********************//
+
+
+
+
+
+// function imgHover (one, two) {
+//   console.log(one);
+//   var art ="art" +one.id[3];
+//   var img = "img" + one.id[3];
+//   if (two ==1) {
+//     var alt = document.getElementById(img).alt;
+//     document.getElementById(art).innerHTML = alt;
+//   } else {
+//     document.getElementById(art).innerHTML = "";
+//   }
+// }
